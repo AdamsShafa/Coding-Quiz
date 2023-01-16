776be5374d01930458a9cf17228b05d6a0c3a38b
AdamsShafa <adamzshafa@gmail.com>
2023-01-16 22:17:56 +0000

    Modified the questions.js file and the highscores.html file

--- JavaScript/questions.js
+++ JavaScript/questions.js
@@ -1,28 +1,28 @@
 // Fomulating the coding quiz questions in an array
 var questions = [
     {
-      question: "Arrays in JavaScript are defined by which of the following statements?",
-      options: ["It is an ordered list of functions", "It is an ordered list of string", "It is an ordered list of objects", "It is an ordered list of values"],
+      title: "Arrays in JavaScript are defined by which of the following statements?",
+      choices: ["It is an ordered list of functions", "It is an ordered list of string", "It is an ordered list of objects", "It is an ordered list of values"],
       correctAnswer: "It is an ordered list of values"
     },
     {
-      question: "Which of the following is not javascript data types?",
-      options: ["Null type", "Number type", "Undefined type", "All of the mentioned"],
+      title: "Which of the following is not javascript data types?",
+      choices: ["Null type", "Number type", "Undefined type", "All of the mentioned"],
       correctAnswer: "All of the mentioned"
     },
     {
-      question: "Where is Client-side JavaScript code embedded within HTML documents?",
-      options: ["A URL that uses the special javascript:encoding", "A URL that uses the special javascript:stack", "A URL that uses the special javascript:code", "URL that uses the special javascript:protocol"],
-      correctAnswer: "A URL that uses the special javascript:protocol"
+      title: "Where is Client-side JavaScript code embedded within HTML documents?",
+      choices: ["A URL that uses the special javascript:encoding", "A URL that uses the special javascript:stack", "A URL that uses the special javascript:code", "URL that uses the special javascript:protocol"],
+      answer: "A URL that uses the special javascript:protocol"
     },
     {
-      question: "Which of the following object is the main entry point to all client-side JavaScript features and APIs?",
-      options: ["Window", "Location", "Position", "Standard"],
-      correctAnswer: "Window"
+      title: "Which of the following object is the main entry point to all client-side JavaScript features and APIs?",
+      choices: ["Window", "Location", "Position", "Standard"],
+      answer: "Window"
     },
     {
-      question: "Which of the following can be used to call a JavaScript Code Snippet?",
-      options: ["Preprocessor", "RMI", "Function/Method", "Triggering Event"],
-      correctAnswer: "Function/Method"
+      title: "Which of the following can be used to call a JavaScript Code Snippet?",
+      choices: ["Preprocessor", "RMI", "Function/Method", "Triggering Event"],
+      answer: "Function/Method"
     }
   ];
